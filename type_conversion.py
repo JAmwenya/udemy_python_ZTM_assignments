ce4c74b9eb74a80b6d9a22a11b47b84c252ea52a
JAmwenya <jnehondo@gmail.com>
2023-01-23 19:41:06 +0300

    type conversion

--- type_conversion.py
+++ type_conversion.py
@@ -1,5 +1,6 @@
 current_year = 2023
 birth_year = input('what year were you born? ')
+#convert the string 'birth_year' to an interger
 birth_year = int(birth_year)
 #age = current_year - birth_year
 print(f"you are now {current_year - birth_year}, years old")
