d50eab9e9ce3ba4efddb82d3a0cb0ed4e78245ee
JAmwenya <jnehondo@gmail.com>
2023-01-23 19:01:35 +0300

    1_type_conversion

---
+++ type_conversion.py
@@ -0,0 +1,5 @@
+current_year = 2023
+birth_year = input('what year were you born? ')
+birth_year = int(birth_year)
+#age = current_year - birth_year
+print(f"you are now {current_year - birth_year}, years old")
